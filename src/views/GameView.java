f7f4c4db50c2ea1d0e94c2d74b44c1e0e67ebc50
HyunJinNo <nhj9831@naver.com>
2023-11-15 18:17:53 +0900

    게임 화면에 구분선 추가

--- src/views/GameView.java
+++ src/views/GameView.java
@@ -41,7 +41,7 @@ public class GameView extends JPanel {
     	levelLabel.setForeground(Color.WHITE);
     	levelLabel.setBackground(Color.BLUE);
     	levelLabel.setOpaque(true);
-    	levelLabel.setBounds(1280, 20, 100, 50);
+    	levelLabel.setBounds(1260, 20, 120, 50);
     	add(levelLabel);
     	
     	timeLabel = new JLabel("남은 시간");
@@ -50,7 +50,7 @@ public class GameView extends JPanel {
     	timeLabel.setForeground(Color.WHITE);
     	timeLabel.setBackground(Color.BLUE);
     	timeLabel.setOpaque(true);
-    	timeLabel.setBounds(1280, 80, 100, 35);
+    	timeLabel.setBounds(1260, 80, 120, 35);
     	add(timeLabel);
     	
     	timeStampLabel = new JLabel("05:00");
@@ -59,8 +59,20 @@ public class GameView extends JPanel {
     	timeStampLabel.setForeground(Color.WHITE);
     	timeStampLabel.setBackground(Color.BLUE);
     	timeStampLabel.setOpaque(true);
-    	timeStampLabel.setBounds(1280, 115, 100, 35);
+    	timeStampLabel.setBounds(1260, 115, 120, 35);
     	add(timeStampLabel);
+    	
+    	JLabel verticalLine = new JLabel();
+    	verticalLine.setBackground(Color.BLACK);
+    	verticalLine.setOpaque(true);
+    	verticalLine.setBounds(1235, 0, 5, 700);
+    	add(verticalLine);
+    	
+    	JLabel horizontalLine = new JLabel();
+    	horizontalLine.setBackground(Color.BLACK);
+    	horizontalLine.setOpaque(true);
+    	horizontalLine.setBounds(0, 460, 1235, 5);
+    	add(horizontalLine);
     	
     	/*
         buttons = new JButton[4];
@@ -107,13 +119,13 @@ public class GameView extends JPanel {
     				textField.setHorizontalAlignment(JTextField.CENTER);
     				textField.setBackground(Color.WHITE);
     				textField.setOpaque(true);
-    				textField.setBounds(i * 30, j * 30, 25, 25);
+    				textField.setBounds(i * 30 + 5, j * 30 + 5, 25, 25);
     				add(textField);
     			} else {
     				JLabel label = new JLabel();
     				label.setBackground(Color.GRAY);
     				label.setOpaque(true);
-    				label.setBounds(i * 30, j * 30, 25, 25);
+    				label.setBounds(i * 30 + 5, j * 30 + 5, 25, 25);
     				add(label);
     			}
     		}

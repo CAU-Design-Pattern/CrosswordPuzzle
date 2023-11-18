b454a2409878bc64cd6bf025ec81384f2e116fa8
Dong <chlehdrb0513@gmail.com>
2023-11-18 22:08:21 +0900

    intellj test 와 eclipse test 를 위한 설정 변경

--- src/test/service/WordServiceTest.java
+++ src/test/service/WordServiceTest.java
@@ -1,13 +1,16 @@
-package service;
+package test.service;
 
 import com.holub.database.Database;
 import com.holub.text.ParseFailure;
 import connector.DatabaseConnector;
+import dto.Word;
 import org.junit.Before;
 import org.junit.Test;
 import repository.WordRepository;
+import service.WordService;
 
 import java.io.IOException;
+import java.util.List;
 
 public class WordServiceTest {
 
@@ -22,8 +25,11 @@ public class WordServiceTest {
     }
 
     @Test
-    public void test(){
+    public void test() throws IOException, ParseFailure {
         // transaction ??? - > 현재 repo 계층에서 dump를 진행중이라 불가?
-
+        List<Word> wordList = wordRepository.getWordList();
+        for (Word word : wordList) {
+            System.out.println("word = " + word);
+        }
     }
 }

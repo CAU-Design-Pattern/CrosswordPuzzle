b2c963d20735cb2f96a4dfa1ff9435974b58c914
Dong <chlehdrb0513@gmail.com>
2023-11-26 23:51:35 +0900

    단어 테스트

--- src/test/WordServiceTest.java
+++ src/test/WordServiceTest.java
@@ -5,6 +5,7 @@ import com.holub.text.ParseFailure;
 import connector.DatabaseConnector;
 import dto.Word;
 import exception.UnsupportedExtensionException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import repository.WordRepository;
@@ -26,13 +27,18 @@ public class WordServiceTest {
     }
 
     @Test
-    public void test() throws IOException, ParseFailure {
-        // transaction ??? - > 현재 repo 계층에서 dump를 진행중이라 불가?
-        List<Word> wordList = wordService.getAllWordList();
-        for (Word word : wordList) {
-            System.out.println("word = " + word);
-        }
+    public void WordTest() throws IOException, ParseFailure {
+//        List<Word> wordList = wordService.getAllWordList();
+//        for (Word word : wordList) {
+//            System.out.println("word = " + word);
+//        }
 
+        wordService.insertOneWord(new Word("myname", "test", 3));
+        Word oneWord = wordService.getOneWord("myname");
+        System.out.println("oneWord = " + oneWord);
+        Assertions.assertEquals("test", oneWord.getDesc());
+
+        // random이라 별다른 assert x
         Word word1 = wordService.getRandomWord('o', 3);
         System.out.println("word1 = " + word1);
         Word word2 = wordService.getRandomWord('r', 4);
@@ -44,12 +50,12 @@ public class WordServiceTest {
         Word word4 = wordService.getRandomWord('u', 2, 3);
         System.out.println("word4 = " + word4);
 
-        System.out.println("-------- migration.csv 에 있는 단어를 import합니다. --------");
-        try{
-            String fileName= "C:/dp2023/migration";
-            wordService.importWordList(fileName, "csv");
-        } catch (UnsupportedExtensionException e){
-            e.printStackTrace();
-        }
+//        System.out.println("-------- migration.csv 에 있는 단어를 import합니다. --------");
+//        try{
+//            String fileName= "C:/dp2023/migration";
+//            wordService.importWordList(fileName, "csv");
+//        } catch (UnsupportedExtensionException e){
+//            e.printStackTrace();
+//        }
     }
 }
